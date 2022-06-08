82cd3ba4cf07b910b8c6ca85f25496a54131009b
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-06-08 21:36:20 +0000

    Initial commit :D

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+
+#include "main.h"
+
+using namespace styling;
+
+void styling::txtred(std::string s) {
+    std::cout << "\033[1;31m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtgreen(std::string s) {
+    std::cout << "\033[1;32m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtblue(std::string s) {
+    std::cout << "\033[1;34m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtyellow(std::string s) {
+    std::cout << "\033[1;33m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtcyan(std::string s) {
+    std::cout << "\033[1;36m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtwhite(std::string s) {
+    std::cout << "\033[1;37m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtblack(std::string s) {
+    std::cout << "\033[1;30m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtbold(std::string s) {
+    std::cout << "\033[1m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtunderline(std::string s) {
+    std::cout << "\033[4m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtblink(std::string s) {
+    std::cout << "\033[5m" << s << "\033[0m" << std::endl;
+}
+
+void styling::txtlinethrough(std::string s) {
+    std::cout << "\033[9m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgred(std::string s) {
+    std::cout << "\033[41m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bggreen(std::string s) {
+    std::cout << "\033[42m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgblue(std::string s) {
+    std::cout << "\033[44m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgyellow(std::string s) {
+    std::cout << "\033[43m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgcyan(std::string s) {
+    std::cout << "\033[46m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgwhite(std::string s) {
+    std::cout << "\033[47m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgblack(std::string s) {
+    std::cout << "\033[40m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bgmagenta(std::string s) {
+    std::cout << "\033[45m" << s << "\033[0m" << std::endl;
+}
+
+void styling::bggrey(std::string s) {
+    std::cout << "\033[100m" << s << "\033[0m" << std::endl;
+}
+
+int main() {
+    txtred("Hello World!");
+    txtgreen("Hello World!");
+    txtblue("Hello World!");
+    txtyellow("Hello World!");
+    txtcyan("Hello World!");
+    txtwhite("Hello World!");
+    txtblack("Hello World!");
+    txtbold("Hello World!");
+    txtunderline("Hello World!");
+    txtblink("Hello World!");
+    txtlinethrough("Hello World!");
+
+    bgred("Hello World!");
+    bggreen("Hello World!");
+    bgblue("Hello World!");
+    bgyellow("Hello World!");
+    bgcyan("Hello World!");
+    bgwhite("Hello World!");
+    bgblack("Hello World!");
+    bgmagenta("Hello World!");
+    bggrey("Hello World!");
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,32 @@
+//
+// Created by will on 08/06/22.
+//
+
+#ifndef CPPSTYLING_MAIN_H
+#define CPPSTYLING_MAIN_H
+
+namespace styling
+{
+    void txtred(std::string s);
+    void txtgreen(std::string s);
+    void txtblue(std::string s);
+    void txtyellow(std::string s);
+    void txtcyan(std::string s);
+    void txtwhite(std::string s);
+    void txtblack(std::string s);
+    void txtbold(std::string s);
+    void txtunderline(std::string s);
+    void txtblink(std::string s);
+    void bgred(std::string s);
+    void bggreen(std::string s);
+    void bgblue(std::string s);
+    void bgyellow(std::string s);
+    void bgcyan(std::string s);
+    void bgwhite(std::string s);
+    void bgblack(std::string s);
+    void bgmagenta(std::string s);
+    void bggrey(std::string s);
+    void txtlinethrough(std::string s);
+}
+
+#endif //CPPSTYLING_MAIN_H
